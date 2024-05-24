b7074a62639ea88f20c8f37054eeeef6ae96b9af
afonsofarias <afonsojansen@gmail.com>
2024-05-24 15:15:21 -0300

    Initial commit

---
+++ src/soma.java
@@ -0,0 +1,6 @@
+public class soma {
+    public static void main (String[] args){
+        int resultado = 10 + 5;
+        System.out.print("O resultado da soma de 10 + 5 e: " + resultado);
+    }
+}
---
+++ src/subtracao.java
@@ -0,0 +1,6 @@
+public class subtracao {
+    public static void main (String[] args) {
+        int resultado = 10 - 5;
+        System.out.print("O resultado da subtração de 10 - 5 é: "+resultado);
+    }
+}
